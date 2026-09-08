00d5ec8bec378d3dd6bdefe6deccab34e180a636
gurtejlamba <lambagurtejsingh@gmail.com>
2026-09-08 23:56:48 +0530

    set compileSdk and targetSdk to 26

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.hydrocare.app"
-    compileSdk = 35
+    compileSdk = 26
 
     defaultConfig {
         applicationId = "com.hydrocare.app"
         minSdk = 26
-        targetSdk = 35
+        targetSdk = 26
         versionCode = 1
         versionName = "1.0"
     }
